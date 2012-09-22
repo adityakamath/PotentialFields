99e5d324678d8644e5a18415208ea509d7eda451
hitokazm <hitokazu@gmail.com>
2012-09-22 11:21:08 -0600

    cant pull why

--- PotentialFields/src/agent.py
+++ PotentialFields/src/agent.py
@@ -4,6 +4,19 @@ Created on Sep 22, 2012
 @author: hitokazu
 '''
 
+import random
+
+time_amount = 0
+
+
 class Agent(object):
-    def __init__():
+    def __init__(self):
         pass
+    
+    def check_time(self, time_diff):
+        limit = random.randint(5) + 3
+        time_amount += time_diff
+        if time_amount >= limit:
+            # Turn left abount 60 degrees
+            time_amount = 0
+        
